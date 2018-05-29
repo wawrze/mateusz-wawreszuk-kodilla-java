423ca7114b1a2ac5f18a2b037a27d5d4bf06c60c
wawrze <mateusz.wawreszuk@gmail.com>
2018-05-29 18:39:08 +0200

    9.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/TitlesIteration.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/TitlesIteration.java
@@ -1,12 +1,15 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.util.stream.Collectors;
+
 public class TitlesIteration {
 
     public static void main (String args[]){
         MovieStore movieStore = new MovieStore();
-        movieStore.getMovies().entrySet().stream()
+        String s = movieStore.getMovies().entrySet().stream()
                 .flatMap(entry -> entry.getValue().stream())
-                .forEach(title -> System.out.print(title + "!"));
+                .collect(Collectors.joining("!"));
+        System.out.println(s);
     }
 
 }
