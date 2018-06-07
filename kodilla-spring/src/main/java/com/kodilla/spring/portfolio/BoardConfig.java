348895917619f30893ede75f4912593bc121173d
wawrze <mateusz.wawreszuk@gmail.com>
2018-06-07 21:42:39 +0200

    10.3 second approach

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,9 +1,28 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
 public class BoardConfig {
+
+    @Autowired
+    @Qualifier("toDoList")
+    TaskList toDoList;
+
+    @Autowired
+    @Qualifier("inProgressList")
+    TaskList inProgressList;
+
+    @Autowired
+    @Qualifier("doneList")
+    TaskList doneList;
+
+    @Bean
+    public Board getBoard() {
+        return new Board(toDoList, inProgressList, doneList);
+    }
 
     @Bean(name = "toDoList")
     @Scope("prototype")
@@ -21,11 +40,6 @@ public class BoardConfig {
     @Scope("prototype")
     public TaskList getDoneList() {
         return new TaskList();
-    }
-
-    @Bean
-    public Board Board() {
-        return new Board(getToDoList(), getInProgressList(), getDoneList());
     }
 
 }
